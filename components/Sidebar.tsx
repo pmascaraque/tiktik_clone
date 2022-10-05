9140aaad41ca8fac4968873e338289eeb43f063e
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-05 04:41:04 +0200

    video next fetching and typing

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,13 +20,12 @@ const Sidebar = () => {
   return (
     <div>
       <div
-        className='block xl:hidden m-1 ml-1 mt-3 text-xl'
+        className='block xl:hidden m-2 ml-4 mt-3 text-xl'
         onClick={() => setShowSidebar((prev) => !prev)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div>
           <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
             <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
               <Link href='/'>
@@ -38,7 +37,6 @@ const Sidebar = () => {
                 </div>
               </Link>
             </div>
-          </div>
           {!userProfile && (
             <div className='px-2 py-4 hidden xl:block'>
               <p className='text-gray-400'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import type { NextPage } from 'next'
+import axios from "axios";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-     TokTok
-    </div>
-  )
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length
+        ? videos.map((video: Video) => (
+            <VideoCard post={video} key={video._id} />
+          ))
+        : (
+          <NoResults text={'No videos'}/>
+        )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: { videos: data },
+  };
+};
+
+export default Home;
