3a057962c043836c8b0e6aa4ef071f0eb119b5be
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-06 04:01:45 +0200

    part 2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['static.wikia.nocookie.net']
+    domains: ['static.wikia.nocookie.net', 'lh3.googleusercontent.com']
   },
   swcMinify: true,
 }
