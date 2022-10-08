6e657b454c19b4c746ea89aece62731f558f804d
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-08 13:39:59 +0200

    like button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -9,29 +9,25 @@ interface IProps {
   handleDislike: () => void;
 }
 
-const LikeButton = ( handleLike, handleDislike ) => {
+const LikeButton = ({ handleLike, handleDislike }: IProps) => {
   const [alreadyLiked, setAlreadyLiked] = useState(true);
   const { userProfile }: any = useAuthStore();
-
-  const handleLike = async(like: boolean) => {
-    if(userProfile) {
-      const response = await axios.put(`${BASE_URL}/api/like`, {
-        userId: userProfile._id,
-        postId: post._id,
-        like
-      })
-    }
-  }
 
   return (
     <div className={`${flex} gap-6`}>
       <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
         {alreadyLiked ? (
-          <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] ' onClick={handleDislike} >
+          <div
+            className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '
+            onClick={handleDislike}
+          >
             <MdFavorite className='text-lg md:text-2xl' />
           </div>
         ) : (
-          <div className='bg-primary rounded-full p-2 md:p-4 ' onClick={handleLike} >
+          <div
+            className='bg-primary rounded-full p-2 md:p-4 '
+            onClick={handleLike}
+          >
             <MdFavorite className='text-lg md:text-2xl' />
           </div>
         )}
