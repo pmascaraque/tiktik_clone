a494eee7e8b8cbdd6bffe4c1e25bf0710682f0cd
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-09 01:35:11 +0200

    suggested accounts

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -21,7 +21,23 @@ const SuggestedAccounts = () => {
         {allUsers.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
             <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
-              <div className="w-8 h-8"></div>
+              <div className='w-8 h-8'>
+                <Image
+                  src={user.image}
+                  width={34}
+                  height={34}
+                  className='rounded-full'
+                  alt='user profile'
+                  layout='responsive'
+                />
+              </div>
+              <div className='hidden xl:block'>
+                <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+                  {user.userName.replaceAll(" ", "")}
+                  <GoVerified className='text-blue-400' />
+                </p>
+                <p className="capitalize text-gray-400 text-xs">{user.userName}</p>
+              </div>
             </div>
           </Link>
         ))}
