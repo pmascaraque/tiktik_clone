75f00d5fa31b866bbed58b5204d571050c98fdc3
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-11 06:10:19 +0200

    nextconfig wikimedia image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['static.wikia.nocookie.net', 'lh3.googleusercontent.com']
+    domains: ['static.wikia.nocookie.net', 'lh3.googleusercontent.com', 'https://upload.wikimedia.org/']
   },
   swcMinify: true,
 }
