bfc7e4f3fbec0443ff2a718a5b2288817a2d66e1
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-19 19:57:37 +0200

    fetching videos query pt1

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { GoVerified } from 'react-icons/go';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
 
-import VideoCard from '../../components/VideoCard';
-import NoResults from '../../components/NoResults';
-import { IUser, Video } from '../../types';
-import { BASE_URL } from '../../utils';
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { IUser, Video } from "../../types";
+import { BASE_URL } from "../../utils";
 
 interface IProps {
   data: {
@@ -21,8 +21,8 @@ const Profile = ({ data }: IProps) => {
   const [videosList, setVideosList] = useState<Video[]>([]);
 
   const { user, userVideos, userLikedVideos } = data;
-  const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
-  const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+  const videos = showUserVideos ? "border-b-2 border-black" : "text-gray-400";
+  const liked = !showUserVideos ? "border-b-2 border-black" : "text-gray-400";
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -52,7 +52,7 @@ const Profile = ({ data }: IProps) => {
 
         <div>
           <div className='text-md md:text-2xl font-bold tracking-wider flex gap-2 items-center justify-center lowercase'>
-            <span>{user.userName.replace(/\s+/g, '')} </span>
+            <span>{user.userName.replace(/\s+/g, "")} </span>
             <GoVerified className='text-blue-400 md:text-xl text-md' />
           </div>
           <p className='text-sm font-medium'> {user.userName}</p>
@@ -60,10 +60,16 @@ const Profile = ({ data }: IProps) => {
       </div>
       <div>
         <div className='flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full'>
-          <p className={`text-xl font-semibold cursor-pointer ${videos} mt-2`} onClick={() => setShowUserVideos(true)}>
+          <p
+            className={`text-xl font-semibold cursor-pointer ${videos} mt-2`}
+            onClick={() => setShowUserVideos(true)}
+          >
             Videos
           </p>
-          <p className={`text-xl font-semibold cursor-pointer ${liked} mt-2`} onClick={() => setShowUserVideos(false)}>
+          <p
+            className={`text-xl font-semibold cursor-pointer ${liked} mt-2`}
+            onClick={() => setShowUserVideos(false)}
+          >
             Liked
           </p>
         </div>
@@ -74,7 +80,7 @@ const Profile = ({ data }: IProps) => {
             ))
           ) : (
             <NoResults
-              text={`No ${showUserVideos ? '' : 'Liked'} Videos Yet`}
+              text={`No ${showUserVideos ? "" : "Liked"} Videos Yet`}
             />
           )}
         </div>
